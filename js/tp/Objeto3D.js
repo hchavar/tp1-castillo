e2a9d87f859a9bde2b8e52df4ad4ce019a60bb3f
Hugo Chavar <hechavar@gmail.com>
2022-10-20 15:44:30 -0300

    Agrego inicializacion de Objeto3D

--- js/tp/Objeto3D.js
+++ js/tp/Objeto3D.js
@@ -14,12 +14,17 @@ class Objeto3D {
         this.width = width;
         this.height = height;
         if (!this.isEmpty()) {
+            this.init();
             this.generateSurface(this.width, this.height);
         }
     }
 
     getName() {
         return "Objeto3D";
+    }
+
+    init() {
+        // inicialiciar objeto
     }
 
     isEmpty() {
@@ -43,6 +48,8 @@ class Objeto3D {
     }
 
     setPosition(position) {
+        // reseteo la matriz
+        this.localMatrix = mat4.create();
         this.pos = position;
     }
 
@@ -63,6 +70,8 @@ class Objeto3D {
     }
 
     setRotation(rotation) {
+        // reseteo la matriz
+        this.localMatrix = mat4.create();
         this.rot = rotation;
     }
 
@@ -133,30 +142,30 @@ class Objeto3D {
         let colors = [];
         let uvList = [];
     
-        for (var i = 0; i <= rows; i++) {
-            for (var j = 0; j <= cols; j++) {
+        for (let i = 0; i <= rows; i++) {
+            for (let j = 0; j <= cols; j++) {
     
-                var u = j/cols;
-                var v = i/rows;
+                let u = j/cols;
+                let v = i/rows;
     
-                var pos = this.getPosition(u, v);
+                let pos = this.getPosition(u, v);
     
                 positions.push(pos[0]);
                 positions.push(pos[1]);
                 positions.push(pos[2]);
     
-                var nrm = this.getNormal(u, v);
+                let nrm = this.getNormal(u, v);
     
                 normals.push(nrm[0]);
                 normals.push(nrm[1]);
                 normals.push(nrm[2]);
     
-                var uvs = this.getTextureCoordinates(u, v);
+                let uvs = this.getTextureCoordinates(u, v);
     
                 uvList.push(uvs[0]);
                 uvList.push(uvs[1]);
     
-                var col = this.getColor(u, v);
+                let col = this.getColor(u, v);
     
                 colors.push(col[0]);
                 colors.push(col[1]);
@@ -168,7 +177,7 @@ class Objeto3D {
         // create index buffer
         let indexes = [];
     
-        for (i = 0; i < rows; i++) {
+        for (let i = 0; i < rows; i++) {
             let firstThisRow = i*(cols + 1);
             let firstNextRow = (i + 1)*(cols + 1);
             let nextRow = -1;
@@ -176,7 +185,7 @@ class Objeto3D {
             if (i > 0)
                 indexes.push(firstThisRow);
             
-            for (j = 0; j <= cols; j++) {
+            for (let j = 0; j <= cols; j++) {
                 indexes.push(j + firstThisRow);
                 nextRow = j + firstNextRow;
                 indexes.push(nextRow);
