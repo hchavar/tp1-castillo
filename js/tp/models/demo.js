ed1aa5c26f24ef2390b9aa6ef1f0ce7599b4af4b
Hugo Chavar <hechavar@gmail.com>
2022-10-20 15:46:51 -0300

    Correccion de objeto Demo

--- js/tp/models/demo.js
+++ js/tp/models/demo.js
@@ -1,55 +1,65 @@
 class Demo extends Objeto3D {
     
-    rotateAngle = -1.57078;
+    rotateAngle = 0;
 
-    constructor(height, width) {
-        super();
-        this.height = height;
-        this.width = width;
+    constructor(width, height) {
+        super(width, height);
     }
 
     isEmpty() {
         return false;
     }
+
+    getName() {
+        return "Demo";
+    }
     
     getPosition(u, v) {
 
-        let r = 2;
-        let nx = Math.sin(v)*Math.sin(u);
-        let ny = Math.sin(v)*Math.cos(u);
-        let nz = Math.cos(v);
+        let x, y, z, r;
+        
+        let alfa = (v * this.height)/(this.height - 1) * Math.PI * 2;
+        let beta = (0.1 + (this.width * u)/(this.width - 1) * 0.8) * Math.PI;
+        
+        r = 2;
+        let nx = Math.sin(beta) * Math.sin(alfa);
+        let ny = Math.sin(beta) * Math.cos(alfa);
+        let nz = Math.cos(beta);
     
-        let g = v % 0.5;
-        let h = u % 1;
+        let g = beta % 0.5;
+        let h = alfa % 1;
         let f = 1;
     
         if (g < 0.25) f = 0.95;
         if (h < 0.5) f = f * 0.95;
     
-        let x = nx * r * f;
-        let y = ny * r * f;
-        let z = nz * r * f;
-    
+        x = nx * r * f;
+        y = ny * r * f;
+        z = nz * r * f;
+
         return [x, y, z];
     }
 
     getNormal(u, v) {
-        var p = this.getPosition(u, v);
-        var v = vec3.create();
-        vec3.normalize(v, p);
-    
-        var delta = 0.05;
-        var p1 = this.getPosition(u, v);
-        var p2 = this.getPosition(u, v + delta);
-        var p3 = this.getPosition(u + delta, v);
-    
-        var v1 = vec3.fromValues(p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]);
-        var v2 = vec3.fromValues(p3[0] - p1[0], p3[1] - p1[1], p3[2] - p1[2]);
-    
+        let alfa = (v * this.height)/(this.height - 1) * Math.PI * 2;
+        let beta = (0.1 + (this.width * u)/(this.width - 1) * 0.8) * Math.PI;
+
+        let p = this.getPosition(alfa, beta);
+        let aVec = vec3.create();
+        vec3.normalize(aVec, p);
+
+        let delta = 0.05;
+        let p1 = this.getPosition(alfa, beta);
+        let p2 = this.getPosition(alfa, beta + delta);
+        let p3 = this.getPosition(alfa + delta, beta);
+
+        let v1 = vec3.fromValues(p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]);
+        let v2 = vec3.fromValues(p3[0] - p1[0], p3[1] - p1[1], p3[2] - p1[2]);
+
         vec3.normalize(v1, v1);
         vec3.normalize(v2, v2);
-    
-        var n = vec3.create();
+        
+        let n = vec3.create();
         vec3.cross(n, v1, v2);
         vec3.scale(n, n, -1);
         return n;
@@ -60,19 +70,14 @@ class Demo extends Objeto3D {
     }
 
     getColor() {
-        return [1, 1, 0];
+        return [1, 0.2, 0.8];
     }
 
     animate() {
 
-        this.rotateAngle += 0.01;
-        //mat4.identity(modelMatrix);
-        mat4.rotate(this.localMatrix, this.localMatrix, this.rotateAngle, [1.0, 0.0, 1.0]);
-    
-        // mat4.identity(normalMatrix);
-        // mat4.multiply(normalMatrix,viewMatrix,modelMatrix);
-        // mat4.invert(normalMatrix,normalMatrix);
-        // mat4.transpose(normalMatrix,normalMatrix);
+        this.rotateAngle = time * Math.PI / 3600;
+        this.setRotation([this.rotateAngle, [0, 0, 1]]);
+        this.updateRotation();
     
     }
 }
