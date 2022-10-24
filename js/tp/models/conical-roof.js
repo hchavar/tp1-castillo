aa5a54cca967bb40e5106886267d5efe4b10dfd5
Hugo Chavar <hechavar@gmail.com>
2022-10-24 12:37:27 -0300

    Agrego techo de columnas

---
+++ js/tp/models/conical-roof.js
@@ -0,0 +1,35 @@
+class ConicalRoof extends RevolutionSurface {
+    constructor(rows, cols, menu) {
+        
+        super(rows, cols, menu);
+        
+    }
+
+    init() {
+
+        let h = 2.0;
+        let r = 1;
+        let x0 = 0.0;
+        let y0 = 0.0;
+        let z0 = 0.0;
+        let origin = [x0, y0, z0];
+        let head = [x0, h, z0];
+
+        this.pc = [
+            [origin, origin, origin, origin],
+            [[0.85*r, y0, z0], [0.20*r, 0.40*h, z0], [ 0.10*r, 0.86*h, z0], head],
+            [head, head, head, head]
+        ];
+
+        super.init();
+    }
+
+    isEmpty(){
+        return false;
+    }
+
+    getColor() {
+        return [0.3, 0.3, 0.9];
+    }
+
+}
