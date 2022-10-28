2394096176fe2e83423b81eaba0776a03a5c1c8e
Hugo Chavar <hechavar@gmail.com>
2022-10-28 14:10:23 -0300

    Sistema disparo catapulta

---
+++ js/tp/models/catapult/firing-system.js
@@ -0,0 +1,39 @@
+const firingSystemConfig = {
+    arm: {
+        position: [0, 0.0, 0.0],
+        rotation: [Math.PI / 2, [1, 0, 0]],
+    },
+    armFrame: {
+        position: [0.0, 0.0, 0.0],
+        scaleFactor: 3
+    }
+}
+
+class FiringSystem extends Objeto3D {
+    constructor() {
+        super();
+    }
+
+    init() {
+
+        let arm = new Arm();
+        arm.setPosition(firingSystemConfig.arm.position);
+        arm.updateLocalMatrix();
+        arm.build();
+        this.addChild(arm);
+
+        // let counterWeight = new CounterWeight();
+        // counterWeight.setPosition(firingSystemConfig.counterWeight.position);
+        // counterWeight.updateLocalMatrix();
+        // counterWeight.build();
+        // this.addChild(counterWeight);
+
+        let armFrame = new Frame(4, 10);
+        armFrame.setPosition(firingSystemConfig.armFrame.position);
+        armFrame.setScale(firingSystemConfig.armFrame.scaleFactor);
+        armFrame.updateLocalMatrix();
+        armFrame.build();
+        this.addChild(armFrame);
+
+    }
+}
