22cc8da40f9772f492604ab205c268ebb17f5ac2
Hugo Chavar <hechavar@gmail.com>
2022-10-29 12:07:42 -0300

    Separo objeto rueda

--- js/tp/models/catapult/axle/axle.js
+++ js/tp/models/catapult/axle/axle.js
@@ -1,22 +1,13 @@
 const axleConfig = {
-    color: [0.9, 0.5, 0.3],
-    reduction: 0.7,
-    rotation: [Math.PI/2, [1,0,0]],
-    scale: {
-        x: 1.0,
-        y: 0.1,
-        z: 1.0
-    },
     wheels: [
         {
-            position: [0, -1.5, -1.0]
+            position: [0, -1.5, 0.0]
         },
         {
-            position: [0, 1.5, -1.0]
+            position: [0, 1.5, 0.0]
         }
     ],
     beam: {
-        position: [0.0, -3.2, -1.0],
         color: [0, 0, 0],
         scale: {
             x: 0.07,
@@ -34,17 +25,13 @@ class Axle extends Objeto3D {
 
     init() {
         
-        for (let wheelConfig of axleConfig.wheels) {
+        for (let wc of axleConfig.wheels) {
 
-            let wheel = new Cylinder(4, 20);
-            wheel.scale = axleConfig.scale;
-    
-            wheel.setRotation(axleConfig.rotation);
-            wheel.setPosition(wheelConfig.position);
+            let wheel = new Wheel();
+            wheel.setRotation(wheelConfig.rotation);
+            wheel.setPosition(wc.position);
             wheel.updateLocalMatrix();
-    
-            wheel.color = axleConfig.color;
-       
+           
             wheel.build();
             this.addChild(wheel);
         }
@@ -52,7 +39,6 @@ class Axle extends Objeto3D {
         let beam = new Cylinder(4, 10);
         beam.scale = axleConfig.beam.scale;
         beam.color = axleConfig.beam.color;
-        beam.setPosition(axleConfig.beam.position);
         beam.setRotation(axleConfig.beam.rotation);
         beam.updateLocalMatrix();
         beam.build();
---
+++ js/tp/models/catapult/axle/wheel.js
@@ -0,0 +1,22 @@
+const wheelConfig = {
+    color: [0.9, 0.5, 0.3],
+    rotation: [Math.PI/2, [1,0,0]],
+    scale: {
+        x: 1.0,
+        y: 0.1,
+        z: 1.0
+    }
+};
+
+class Wheel extends Cylinder {
+    constructor() {
+        super(4, 20);
+    }
+
+    init() {
+        this.empty = false;
+        this.scale = wheelConfig.scale;
+        this.color = wheelConfig.color;
+        this.name = "Wheel";
+    }
+}
