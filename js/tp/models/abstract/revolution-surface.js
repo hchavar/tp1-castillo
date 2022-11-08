34899268909b66b50e7774b6ff4de53938dd8973
Hugo Chavar <hechavar@gmail.com>
2022-11-08 02:13:40 -0300

    Normales superficie revolucion

--- js/tp/models/abstract/revolution-surface.js
+++ js/tp/models/abstract/revolution-surface.js
@@ -1,38 +1,79 @@
 class RevolutionSurface extends BezierSurface {
-    
+
     constructor(rows, cols, menu) {
-        
+
         super(rows, cols, menu);
-        
+
     }
 
-    getPosition(u,v) {
-        
+    getPosition(u, v) {
+
+        let pathBinormal = vec3.fromValues(0, 1, 0);
+
+        let pathNormal = vec3.fromValues(1, 0, 0);
+        let pathTangent = vec3.fromValues(0, 0, 1);
+
+        let angle = v * 2.0 * Math.PI;
+
+        vec3.rotateY(pathNormal, pathNormal, [0, 1, 0], angle);
+        vec3.rotateY(pathTangent, pathTangent, [0, 1, 0], angle);
+
+        let pathPosition = vec3.fromValues(0, 0, 0);
+
+        let mat = glMatrix.mat4.fromValues(
+            pathNormal[0], pathBinormal[0], pathTangent[0], pathPosition[0],
+            pathNormal[1], pathBinormal[1], pathTangent[1], pathPosition[1],
+            pathNormal[2], pathBinormal[2], pathTangent[2], pathPosition[2],
+            0, 0, 0, 1
+        );
+
         let pos = Math.round(u * (this.curve.points.length - 1));
-      
-        let pc = this.curve.points[pos];
-        
-        let p = [pc[0], pc[1], pc[2], 0];
 
-        let angulo = v *2.0 * Math.PI; 
+        let pc = vec3.clone(this.curve.points[pos]);
+        let p2 = [pc[0], pc[1], pc[2], 0];
 
-        vec3.rotateY(p, p, [0, 1, 0], angulo);
+        glMatrix.vec3.transformMat4(pc, pc, mat);
 
-        return p;
+        return pc;
     }
 
-    getNormal(u,v) {
-        return this.getPosition(u, v);
+    getNormal(u, v) {
+        let pathBinormal = vec3.fromValues(0, 1, 0);
+
+        let pathNormal = vec3.fromValues(1, 0, 0);
+        let pathTangent = vec3.fromValues(0, 0, 1);
+
+        let angle = v * 2.0 * Math.PI;
+
+        vec3.rotateY(pathNormal, pathNormal, [0, 1, 0], angle);
+        vec3.rotateY(pathTangent, pathTangent, [0, 1, 0], angle);
+
+        let pathPosition = vec3.fromValues(0, 0, 0);
+
+        let mat = glMatrix.mat4.fromValues(
+            pathNormal[0], pathBinormal[0], pathTangent[0], pathPosition[0],
+            pathNormal[1], pathBinormal[1], pathTangent[1], pathPosition[1],
+            pathNormal[2], pathBinormal[2], pathTangent[2], pathPosition[2],
+            0, 0, 0, 1
+        );
+
+        let pos = Math.round(u * (this.curve.points.length - 1));
+
+        let pn = vec3.clone(this.curve.normals[pos]);
+        glMatrix.vec3.transformMat4(pn, pn, mat);
+
+        return pn;
+
     }
-    
-    getTextureCoordinates(u,v) {
-        return [u,v];
+
+    getTextureCoordinates(u, v) {
+        return [u, v];
     }
 
     animate() {
 
         let rotateAngle = time * Math.PI / 30;
         this.setRotation([rotateAngle, [0, 1, 0]]);
-    
+
     }
 }
