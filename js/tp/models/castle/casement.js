7ce0c44a8c30b50f4830d688102544d00d5f528f
Hugo Chavar <hechavar@gmail.com>
2022-11-08 23:06:59 -0300

    Correccion normales ventanas

--- js/tp/models/castle/casement.js
+++ js/tp/models/castle/casement.js
@@ -1,7 +1,7 @@
 class Casement extends Objeto3D {
 
     constructor(menu) {
-        super(4, 32, menu);
+        super(6, 32, menu);
     }
 
     static create() {
@@ -40,6 +40,16 @@ class Casement extends Objeto3D {
             z = defaultCenter.z;
         } else {
 
+            let newV = v;
+            // las penultimas 2 filas las mando al mismo nivel para 
+            // que tengan la misma normal
+            if (v >= (this.width - 2) / this.width) {
+                newV = (this.width - 1) / this.width;
+            }
+            if (v <= 2 / this.width) {
+                newV = 1 / this.width;
+            }
+
             if (u < 0.25) {
                 x = 0.0;
                 z = 4 * u;
@@ -61,14 +71,39 @@ class Casement extends Objeto3D {
                 x = 1 - 4 * (u - 0.75);
                 
             }
-            y = (v - 1 / this.width) / (0.5 - 1 / this.width) * 0.5;
+            y = (newV - 1 / this.width) / (0.5 - 1 / this.width) * 0.5;
         }
 
         return [(x - defaultCenter.x) * this.scale.x, (y - defaultCenter.y) * this.scale.y, (z - defaultCenter.z) * this.scale.z];
     }
 
     getNormal(u, v) {
-        return this.getPosition(u, v);
+        if (this.isTopFace(v)) {
+            return [0, 1, 0];
+        } else if (this.isBottomFace(v)) {
+            return [0, -1, 0];
+        } else {
+
+            if (u <= 0.25) {
+                return [-1, 0, 0];
+            } else if (u <= 0.5) {
+                return [0, 0, 1];
+            } else if (u <= 0.75) {
+                return [1, 0, 0];
+            } else {
+                return [0, 0, -1];
+            }
+
+        }
+
+    }
+
+    isBottomFace(v) {
+        return v <= 1 / this.width;
+    }
+
+    isTopFace(v) {
+        return v >= (this.width - 1) / this.width;
     }
 
     getTextureCoordinates(u, v) {
