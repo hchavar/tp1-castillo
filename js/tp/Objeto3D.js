b9e2052d48a214d138bc12ff54416fc0ac549984
Hugo Chavar <hechavar@gmail.com>
2022-12-01 09:43:24 -0300

    Carga de texturas

--- js/tp/Objeto3D.js
+++ js/tp/Objeto3D.js
@@ -24,6 +24,8 @@ class Objeto3D {
     ambientColor = defaultConfig.ambientColor;
     worldPosition = vec3.create();
     shaderProgram = globalShaderProgram;
+    srcImage = null;
+    texture = null;
 
     // Phong model coefficients
     ka = defaultConfig.reflection.ka;
@@ -387,4 +389,59 @@ class Objeto3D {
     set visible(value) {
         this._visible = value;
     }
+
+    loadTexture() {
+        
+        function isPowerOf2(value) {
+            return (value & (value - 1)) === 0;
+        }
+        
+        this.texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+        const level = 0;
+        const internalFormat = gl.RGBA;
+        const width = 1;
+        const height = 1;
+        const border = 0;
+        const srcFormat = gl.RGBA;
+        const srcType = gl.UNSIGNED_BYTE;
+        const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+        gl.texImage2D(
+            gl.TEXTURE_2D,
+            level,
+            internalFormat,
+            width,
+            height,
+            border,
+            srcFormat,
+            srcType,
+            pixel
+        );
+
+        const image = new Image();
+        image.onload = () => {
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+            gl.texImage2D(
+                gl.TEXTURE_2D,
+                level,
+                internalFormat,
+                srcFormat,
+                srcType,
+                image
+            );
+
+            if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+                gl.generateMipmap(gl.TEXTURE_2D);
+            } else {
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            }
+        };
+        image.src = this.srcImage;
+
+    }
+
+
 }
